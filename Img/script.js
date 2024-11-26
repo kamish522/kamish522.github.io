3cdb658b2d05c99c869c83ee319c4219fef683df
Kamish <ipomainkra@gmail.com>
2024-11-26 12:29:40 +1000

    Delete Img/script.js

--- Img/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('hamburger').addEventListener('click', function() {
-    var menu = document.getElementById('mobile-menu');
-    if (menu.classList.contains('show')) {
-        menu.classList.remove('show');
-    } else {
-        menu.classList.add('show');
-    }
-});
