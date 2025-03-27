84f57aededd9a28a94a426af3352450397b98d00
Mikhail <ipomainkra@gmail.com>
2025-03-27 10:47:49 +1000

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,34 @@ async function initMap() {
         }
     );
 
-    map.addChild(new YMapDefaultSchemeLayer());
+    map.addChild(new YMapDefaultSchemeLayer({
+        customization: [
+    // Делаем прозрачными все геометрии водных объектов.
+    {
+      tags: {
+        all: ['water']
+      },
+      elements: 'geometry',
+      stylers: [
+        {
+          opacity: 0
+        }
+      ]
+    },
+    // Меняем цвет подписей для всех POI и узлов сети общественного транспорта.
+    {
+      tags: {
+        any: ['poi', 'transit_location']
+      },
+      elements: 'label.text.fill',
+      stylers: [
+        {
+          color: '#0000DD'
+        }
+      ]
+    }
+  ]
+    }));
 }
 
 initMap();
